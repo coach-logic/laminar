3059908cfe2bb83797b95913c7e3d37ff86bded1
nverba <distantstate@gmail.com>
2019-08-06 17:28:32 +0100

    gitpkg

---
+++ types/flow.ts
@@ -0,0 +1,35 @@
+export interface Step {
+  id: string;
+  call?: ((any) => any) | Promise<any>;
+  store?: string;
+  dispatch?: {
+    type: string;
+    [key: string]: any;
+  };
+  args?: any;
+  wait?: {
+    on: string;
+    async?: boolean;
+  },
+  pushFlow?: (any) => (any) => {
+    flow: Flow;
+  };
+  saveResponse?: string;
+  useResponse?: string;
+  get?: string;
+  put?: { url: string, body: {
+    [key: string]: any;
+  }};
+  post?: { url: string, body: {
+    [key: string]: any;
+  }};
+  delete?: { url: string, body: {
+    [key: string]: any;
+  }};
+  validate?: (any) => any;  
+  reduce?: (any) => any;  
+}
+
+export interface Flow {
+  [index: number]: Step;
+}
